ac61ee026661f9e07ad2aa581412c89d973a61b9
Cailin Bell Wold <79965490+CailinBellWold@users.noreply.github.com>
2021-09-06 17:19:53 -0700

    Updated reducers, store.

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Provider } from 'react-redux';
+// import React from 'react';
+// import { Provider } from 'react-redux';
 
-// Import Redux store from the file we created in /utils
-import store from './store';
+// // Import Redux store from the file we created in /utils
+// import store from './store';
 
-export default function StoreProvider(props) {
-  // Return the provider component provided by react-redux
-  return <Provider store={store} {...props} />;
-}
+// export default function StoreProvider(props) {
+//   // Return the provider component provided by react-redux
+//   return <Provider store={store} {...props} />;
+// }
 
 // import React, { createContext, useContext } from "react";
 // import { useProductReducer } from './reducers'
--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,4 +1,3 @@
-// import { useReducer } from 'react';
 import {
   UPDATE_PRODUCTS,
   ADD_TO_CART,
@@ -12,27 +11,16 @@ import {
 } from './actions';
 
 //Set up Initial State, as was previously done in Global State
-const initialState = {
+const initalState = {
   products: [],
   cart: [],
   cartOpen: false,
   categories: [],
   currentCategory: '',
-};
+}
 
-// const StoreProvider = ({ value = [], ...props }) => {
-//   const [state, dispatch] = useProductReducer({
-//     products: [],
-//     cart: [],
-//     cartOpen: false,
-//     categories: [],
-//     currentCategory: '',
-//   }); 
-
-// The reducer is a function that accepts the current state and an action. It returns a new state based on that action.
-export const reducer = (state, action) => {
+export default function reducer(state = initalState, action) {
   switch (action.type) {
-    // Returns a copy of state with an update products array. We use the action.products property and spread it's contents into the new array.
     case UPDATE_PRODUCTS:
       return {
         ...state,
@@ -50,8 +38,6 @@ export const reducer = (state, action) => {
         ...state,
         cart: [...state.cart, ...action.products],
       };
-    // Returns a copy of state, sets the cartOpen to true and maps through the items in the cart.
-    // If the item's `id` matches the `id` that was provided in the action.payload, we update the purchase quantity.
     case UPDATE_CART_QUANTITY:
       return {
         ...state,
@@ -63,29 +49,21 @@ export const reducer = (state, action) => {
           return product;
         }),
       };
-
-    // First we iterate through each item in the cart and check to see if the `product._id` matches the `action._id`
-    // If so, we remove it from our cart and set the updated state to a variable called `newState`
     case REMOVE_FROM_CART:
       let newState = state.cart.filter((product) => {
         return product._id !== action._id;
       });
-
-      // Then we return a copy of state and check to see if the cart is empty.
-      // If not, we set the cartOpen status to  `true`. Then we return an updated cart array set to the value of `newState`.
       return {
         ...state,
         cartOpen: newState.length > 0,
         cart: newState,
       };
-
     case CLEAR_CART:
       return {
         ...state,
         cartOpen: false,
         cart: [],
       };
-
     case TOGGLE_CART:
       return {
         ...state,
@@ -97,20 +75,13 @@ export const reducer = (state, action) => {
         ...state,
         categories: [...action.categories],
       };
-
     case UPDATE_CURRENT_CATEGORY:
       return {
         ...state,
         currentCategory: action.currentCategory,
       };
 
-    // Return the state as is in the event that the `action.type` passed to our reducer was not accounted for by the developers
-    // This saves us from a crash.
     default:
       return state;
   }
-};
-
-export function useProductReducer(initialState) {
-  return useReducer(reducer, initialState);
 }
